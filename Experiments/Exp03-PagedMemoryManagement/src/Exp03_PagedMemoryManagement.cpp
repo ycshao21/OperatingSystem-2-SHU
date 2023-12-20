63c8700e8d83b3f0259d524895b4c23243d23a5d
ycshao21 <matthewshao21@outlook.com>
2023-12-21 01:22:14 +0800

    Exp03 - Task02

--- Experiments/Exp03-PagedMemoryManagement/src/Exp03_PagedMemoryManagement.cpp
+++ Experiments/Exp03-PagedMemoryManagement/src/Exp03_PagedMemoryManagement.cpp
@@ -4,16 +4,22 @@
 #include "Random.h"
 #include "PagedMemory.h"
 
-std::vector<uint32_t> GeneratePageReferences(uint32_t refCount)
+enum class AlgorithmType: uint8_t
+{
+    Optimal = 0, FIFO, LRU
+};
+
+std::vector<uint32_t> GeneratePageReferences(uint32_t refCount, int pageCount)
 {
     std::vector<uint32_t> pageRefs(refCount);
     for (int i = 0; i < refCount; ++i)
-        pageRefs[i] = Random::GenerateInt32(0, 6);
+        pageRefs[i] = Random::GenerateInt32(0, pageCount);
     return pageRefs;
 }
 
-void PrintMemoryState(const std::vector<std::vector<uint32_t>>& memoryState, int capacity)
+void PrintMemorySequence(const std::vector<std::vector<uint32_t>>& memoryState, int capacity)
 {
+    std::cout << "Memory Sequence: \n";
     for (int i = 0; i < capacity; ++i)
     {
         for (int j = 0; j < memoryState.size(); ++j)
@@ -27,37 +33,36 @@ void PrintMemoryState(const std::vector<std::vector<uint32_t>>& memoryState, int
     }
 }
 
-int main()
+void Task01()
 {
-    Random::Init();
-
     // Initialize memory.
     PagedMemory memory;
-    uint32_t memorySize = 48, pageSize = 16;
+    uint32_t memorySize = 3, pageSize = 1;
     memory.SetVirtualMemorySize(memorySize);
     memory.SetPageSize(pageSize);
     uint32_t capacity = memorySize / pageSize;
 
     // Generate page references.
-    auto pageRefs = GeneratePageReferences(20);
-    std::cout << "Page references: ";
+    auto pageRefs = GeneratePageReferences(20, 32);
+    std::cout << "Page references:\n";
     for (uint32_t page: pageRefs)
         std::cout << page << " ";
-    std::cout << "\n";
-
+    std::cout << "\n\n";
+    
     {
-        std::cout << "Optimal: \n";
+        std::cout << "-----------\n";
+        std::cout << "| Optimal |\n";
+        std::cout << "-----------\n";
 
-        std::vector<std::vector<uint32_t>> memoryState;
+        std::vector<std::vector<uint32_t>> memorySequence;
         int missCount = 0;
-
         for (int i = 0; i < pageRefs.size(); ++i)
         {
             if (memory.Optimal(pageRefs, i))
                 ++missCount;
-            memoryState.push_back(memory.GetPagesInMemory());
+            memorySequence.push_back(memory.GetPagesInMemory());
         }
-        PrintMemoryState(memoryState, capacity);
+        PrintMemorySequence(memorySequence, capacity);
 
         float missRate = (float)missCount / (float)pageRefs.size();
         std::cout << std::format("miss: {0}/{1}={2}\n", missCount, pageRefs.size(), missRate);
@@ -66,17 +71,19 @@ int main()
     memory.Clear();
 
     {
-        std::cout << "FIFO: \n";
+        std::cout << "--------\n";
+        std::cout << "| FIFO |\n";
+        std::cout << "--------\n";
 
-        std::vector<std::vector<uint32_t>> memoryState;
+        std::vector<std::vector<uint32_t>> memorySequence;
         int missCount = 0;
         for (uint32_t ref : pageRefs)
         {
             if (memory.FIFO(ref))
                 ++missCount;
-            memoryState.push_back(memory.GetPagesInMemory());
+            memorySequence.push_back(memory.GetPagesInMemory());
         }
-        PrintMemoryState(memoryState, capacity);
+        PrintMemorySequence(memorySequence, capacity);
 
         float missRate = (float)missCount / (float)pageRefs.size();
         std::cout << std::format("miss: {0}/{1}={2}\n", missCount, pageRefs.size(), missRate);
@@ -84,21 +91,82 @@ int main()
     std::cout << "\n";
     memory.Clear();
 
-
     {
-        std::cout << "LRU: \n";
+        std::cout << "-------\n";
+        std::cout << "| LRU |\n";
+        std::cout << "-------\n";
 
-        std::vector<std::vector<uint32_t>> memoryState;
+        std::vector<std::vector<uint32_t>> memorySequence;
         int missCount = 0;
         for (uint32_t ref : pageRefs)
         {
             if (memory.LRU(ref))
                 ++missCount;
-            memoryState.push_back(memory.GetPagesInMemory());
+            memorySequence.push_back(memory.GetPagesInMemory());
         }
-        PrintMemoryState(memoryState, capacity);
+        PrintMemorySequence(memorySequence, capacity);
 
         float missRate = (float)missCount / (float)pageRefs.size();
         std::cout << std::format("miss: {0}/{1}={2}\n", missCount, pageRefs.size(), missRate);
     }
+}
+
+void Task02()
+{
+    // Initialize memory.
+    PagedMemory memory;
+    uint32_t pageSize = 1;
+    memory.SetPageSize(pageSize);
+
+    // Generate page references.
+    auto pageRefs = GeneratePageReferences(200, 32);
+
+    for (int memorySize = 2; memorySize <= 32; memorySize += 2)
+    {
+        memory.SetVirtualMemorySize(memorySize);
+        std::cout << std::format("Memory size: {}\n", memorySize);
+
+        {
+            int missCount = 0;
+            for (int i = 0; i < pageRefs.size(); ++i)
+            {
+                if (memory.Optimal(pageRefs, i))
+                    ++missCount;
+            }
+            float hitRate = 1.0f - (float)missCount / (float)pageRefs.size();
+            std::cout << std::format("Optimal  Miss: {0}  Hit rate: {1}\n", missCount, hitRate);
+            memory.Clear();
+        }
+
+        {
+            int missCount = 0;
+            for (uint32_t ref: pageRefs)
+            {
+                if (memory.FIFO(ref))
+                    ++missCount;
+            }
+            float hitRate = 1.0f - (float)missCount / (float)pageRefs.size();
+            std::cout << std::format("FIFO     Miss: {0}  Hit rate: {1}\n", missCount, hitRate);
+            memory.Clear();
+        }
+
+        {
+            int missCount = 0;
+            for (uint32_t ref: pageRefs)
+            {
+                if (memory.LRU(ref))
+                    ++missCount;
+            }
+            float hitRate = 1.0f - (float)missCount / (float)pageRefs.size();
+            std::cout << std::format("LRU      Miss: {0}  Hit rate: {1}\n", missCount, hitRate);
+            memory.Clear();
+        }
+        std::cout << "---------------------------------------------------------------------\n";
+    }
+}
+
+int main()
+{
+    Random::Init();
+    Task02();
 }
